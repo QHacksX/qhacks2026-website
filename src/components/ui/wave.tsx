4483deb319fca6c0903502a02754452aa8bbd9f0
Kosi <kosi.amobioleka@gmail.com>
2025-08-05 14:11:50 -0400

    feat: Waves

---
+++ src/components/ui/wave.tsx
@@ -0,0 +1,11 @@
+export default function Wave() {
+    return (
+        <div className='absolute w-full h-96 overflow-hidden'>
+             <img
+            className='w-full absolute h-full object-cover'
+            src='/waves/wave.svg'
+            alt=''
+          ></img>
+        </div>
+    )
+}
