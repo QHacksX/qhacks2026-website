2084cee4143a1e6809ccb608752ac5621834964e
Kuzey Bilgin <kuzeybilgin@proton.me>
2025-09-16 00:37:14 -0400

    Minor fixes to the clock style

--- src/components/Timer/timer.tsx
+++ src/components/Timer/timer.tsx
@@ -19,10 +19,10 @@ const Timer = ({ days, hours, minutes, seconds }: TimerProps) => {
       {items.map((item, index) => (
         <div key={index} className="flex items-center">
           <div className="flex flex-col items-center">
-            <span className="font-bold sm:text-3xl md:text-3xl lg:text-4xl text-base xl:text-3xl">
+            <span className="font-mono font-bold sm:text-3xl md:text-3xl lg:text-4xl text-base xl:text-3xl">
               {item.value}
             </span>
-            <span className="text-gray-500 mt-1  sm:text-lg md:text-2xl lg:text-2xl text-base xl:text-xl">
+            <span className="text-gray-500 sm:text-lg md:text-2xl lg:text-2xl text-base xl:text-xl">
               {item.label}
             </span>
           </div>
