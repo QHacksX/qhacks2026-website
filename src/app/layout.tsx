5cd2e816d379c4da084a113645b934acf4b9e9ce
Kosi <kosi.amobioleka@gmail.com>
2025-09-15 23:34:06 -0400

    feat: add new logo and update layout icon reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "QHacks 2026",
   description: "Queen's University's annual hackathon is back for it's 10th iteration! Come pre-register for the event in February 2025 and join hundreds of applicants, meet tech companies, and have one of the best experiences ever!",
   keywords: ['QHacks', 'QHacks 2025', 'Queens Hackathon'],
-  icons: '/logo/logo.png',
+  icons: '/logo/5.png',
 };
 
 export default function RootLayout({
